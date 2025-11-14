b8c0c8a31d227a86b0dc6218a8a612e7441740c1
Данил <wanty3@mail.ru>
2025-11-14 14:04:25 +0300

    Тема горутины, задание №1

--- main.go
+++ main.go
@@ -1,96 +1,35 @@
 package main
 
 import (
-	"Poligon/Operator"
-	"bufio"
 	"fmt"
-	"os"
-	"strconv"
+	"sync"
+	"time"
 )
 
 func main() {
 
-	fmt.Println("Ваш банковский счет")
-	cashAccount := 1000
-	for {
-		scanner := bufio.NewScanner(os.Stdin)
-		fmt.Println("Выберете команду" +
-			"\n1 - Выдать наличные" +
-			"\n2 - показать баланс" +
-			"\n3 - провести онлайн оплату" +
-			"\nexit - завершить работу")
+	var wg sync.WaitGroup
 
-		if ok := scanner.Scan(); !ok {
-			fmt.Println("Ошибка ввода")
-			return
-		}
+	wg.Add(3)
 
-		input := scanner.Text()
+	go helloGorutine(1, &wg)
+	go helloGorutine(2, &wg)
+	go helloGorutine(3, &wg)
 
-		err := Operator.ExecutionError()
-		if err != nil {
-			fmt.Println(err)
-			continue
-		}
+	wg.Wait()
 
-		switch input {
-
-		case "1":
-			fmt.Println("Введите сумму ")
-			if ok := scanner.Scan(); !ok {
-				fmt.Println("Ошибка ввода")
-				return
-			}
-			userRequest, err := strconv.Atoi(scanner.Text())
-			if err != nil {
-				fmt.Println("804 -ошибка ввода, некорректное число")
-			}
-			cashAccount, _ = Operator.GiveCash(cashAccount, userRequest)
-
-			fmt.Println("Выдача наличных средств")
-			break
-
-		case "2":
-			fmt.Println("Баланс составляет", cashAccount)
-			break
-
-		case "3":
-			fmt.Println("Введите сумму оплаты")
-			if ok := scanner.Scan(); !ok {
-				fmt.Println("Ошибка ввода")
-				return
-			}
-			userRequest, err := strconv.Atoi(scanner.Text())
-			if err != nil {
-				fmt.Println("804 -ошибка ввода, некорректное число")
-			}
-			cashAccount, _ = Operator.OnlinePayment(cashAccount, userRequest)
-			fmt.Println("Оплата выполнена успешно")
-
-			break
-
-		case "exit":
-			os.Exit(0)
-
-		default:
-			fmt.Println("ошибка 801 - не известная команда")
-		}
-
-	}
-
+	fmt.Println("\nDone")
 }
 
-/*
-Коды ошибок
- 800 - ошибка проведения операции
- 801 - ошибка, не известная команда
- 802 - ошибка, недостаточно средств
- 803 - ошибка ввода, сумма для выдачи не должна быть меньше 10
- 804 - ошибка ввода, некорректное число
- 805 -
- 806 -
- 807 -
- 808 -
- 809 -
- 810 -
-*/
+func helloGorutine(number int, wg *sync.WaitGroup) {
+
+	for i := 0; i < 5; i++ {
+
+		fmt.Println("Я горутина", number, "делаю вывод на экран в", i+1, "раз")
+		time.Sleep(1 * time.Second)
+
+		if i == 4 {
+			wg.Done()
+		}
+	}
+}
