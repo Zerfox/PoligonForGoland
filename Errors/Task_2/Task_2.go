6990f3f50c22d098af52e2675b11da02a4c33625
Данил <wanty3@mail.ru>
2025-11-14 14:44:18 +0300

    Переформирование. Тема горутины. Задание №1

---
+++ Errors/Task_2/Task_2.go
@@ -0,0 +1,95 @@
+package Task_2
+
+import (
+	"bufio"
+	"fmt"
+	"os"
+	"strconv"
+)
+
+func main() {
+
+	fmt.Println("Ваш банковский счет")
+	cashAccount := 1000
+	for {
+		scanner := bufio.NewScanner(os.Stdin)
+		fmt.Println("Выберете команду" +
+			"\n1 - Выдать наличные" +
+			"\n2 - показать баланс" +
+			"\n3 - провести онлайн оплату" +
+			"\nexit - завершить работу")
+
+		if ok := scanner.Scan(); !ok {
+			fmt.Println("Ошибка ввода")
+			return
+		}
+
+		input := scanner.Text()
+
+		err := ExecutionError()
+		if err != nil {
+			fmt.Println(err)
+			continue
+		}
+
+		switch input {
+
+		case "1":
+			fmt.Println("Введите сумму ")
+			if ok := scanner.Scan(); !ok {
+				fmt.Println("Ошибка ввода")
+				return
+			}
+			userRequest, err := strconv.Atoi(scanner.Text())
+			if err != nil {
+				fmt.Println("804 -ошибка ввода, некорректное число")
+			}
+			cashAccount, _ = GiveCash(cashAccount, userRequest)
+
+			fmt.Println("Выдача наличных средств")
+			break
+
+		case "2":
+			fmt.Println("Баланс составляет", cashAccount)
+			break
+
+		case "3":
+			fmt.Println("Введите сумму оплаты")
+			if ok := scanner.Scan(); !ok {
+				fmt.Println("Ошибка ввода")
+				return
+			}
+			userRequest, err := strconv.Atoi(scanner.Text())
+			if err != nil {
+				fmt.Println("804 -ошибка ввода, некорректное число")
+			}
+			cashAccount, _ = OnlinePayment(cashAccount, userRequest)
+			fmt.Println("Оплата выполнена успешно")
+
+			break
+
+		case "exit":
+			os.Exit(0)
+
+		default:
+			fmt.Println("ошибка 801 - не известная команда")
+		}
+
+	}
+
+}
+
+/*
+Коды ошибок
+800 - ошибка проведения операции
+801 - ошибка, не известная команда
+802 - ошибка, недостаточно средств
+803 - ошибка ввода, сумма для выдачи не должна быть меньше 10
+804 - ошибка ввода, некорректное число
+805 -
+806 -
+807 -
+808 -
+809 -
+810 -
+*/
--- main.go
+++ main.go
@@ -1,35 +1,9 @@
 package main
 
 import (
-	"fmt"
-	"sync"
-	"time"
+	"Poligon/Gorutine"
 )
 
 func main() {
-
-	var wg sync.WaitGroup
-
-	wg.Add(3)
-
-	go helloGorutine(1, &wg)
-	go helloGorutine(2, &wg)
-	go helloGorutine(3, &wg)
-
-	wg.Wait()
-
-	fmt.Println("\nDone")
-}
-
-func helloGorutine(number int, wg *sync.WaitGroup) {
-
-	for i := 0; i < 5; i++ {
-
-		fmt.Println("Я горутина", number, "делаю вывод на экран в", i+1, "раз")
-		time.Sleep(1 * time.Second)
-
-		if i == 4 {
-			wg.Done()
-		}
-	}
+	Gorutine.GorutineMain()
 }
