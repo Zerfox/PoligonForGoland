022ef388cbed33d6bb45038d28c9739990a83ecd
Данил <wanty3@mail.ru>
2025-12-14 01:13:20 +0300

    Тема: гонка данных, задание №2 почтовик с спамерами

--- DataRace/DataRaceTask2.go
+++ DataRace/DataRaceTask2.go
@@ -7,28 +7,38 @@ import (
 	"sync"
 )
 
+/*
+В данном задании нужно было реализовать почтовик на который сыпяться сообщения и проверить будет ли гонка данных в этом случае и нужно было проверить есть ли она
+Я прогнал это конкурентном варианте и у меня не появилось гонки данных, если я правильно понимаю, гонки не было из-за того что добавление данных в слайс не требует точности
+Я пробовал начиная с 10 добавлять нули до 10000 и ни в одном случае поиск гонки данных -race не обнаружил у меня гонки
+*/
+
 func DataRaceTask2Main() {
 	wg := &sync.WaitGroup{}
-	mail := make([]string, 100)
+	mail := make([]string, 0, 100)
+	ptMail := &mail
 
-	send(mail, wg)
+	for i := 0; i < 10000; i++ {
+		wg.Add(1)
+		sendMessage(ptMail, wg)
+	}
 
 	wg.Wait()
-	fmt.Println(mail)
+	fmt.Println("Скрипт отработал: ", mail)
 }
 
-func takeOpinion() string {
+func takeString() string {
 	const charset = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ "
 	sb := strings.Builder{}
 	sb.Grow(5)
 	for i := 0; i < 10; i++ {
 		sb.WriteByte(charset[rand.Intn(len(charset))])
 	}
+
 	return sb.String()
 }
 
-func send(mail []string, wg *sync.WaitGroup) {
+func sendMessage(mail *[]string, wg *sync.WaitGroup) {
 	defer wg.Done()
-
-	mail = append(mail, takeOpinion())
+	*mail = append(*mail, takeString())
 }
